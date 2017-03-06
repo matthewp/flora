5efbd9225d92cd52d20b7b662c836b9523a97e7a
Matthew Phillips <matthew@matthewphillips.info>
2017-03-06 07:07:03 -0500

    Conditionals fully work

--- test/test.js
+++ test/test.js
@@ -3,31 +3,57 @@ const flora = require('../lib/fflora');
 const helpers = require('./helpers');
 
 describe('Conditionals', function(){
-  it('basics works', function(done){
-    let tmpl = `
-      <span class="msg">Hello <strong>{{name}}</strong>!</span>
+  describe('Truthy', function(){
+    it('basics works', function(done){
+      let tmpl = `
+        <span class="msg">Hello <strong>{{name}}</strong>!</span>
 
-      <template if="{{hasUser}}">
-        <span>{{handle}}</span>
-      </template>
-    `;
+        <template if="{{hasUser}}">
+          <span>{{handle}}</span>
+        </template>
+      `;
 
-    let data = {
-      name: 'Wilbur',
-      hasUser: Promise.resolve().then(_ => {
-        data.handle = 'w!lbur';
-        return true;
+      let data = {
+        name: 'Wilbur',
+        hasUser: Promise.resolve().then(_ => {
+          data.handle = 'w!lbur';
+          return true;
+        })
+      };
+
+      helpers.render(tmpl)(data).then(parts => {
+        assert.equal(parts.length, 2);
+        assert.equal(parts[0].trim(),
+        '<span class="msg">Hello <strong>Wilbur</strong>!</span>');
+        assert.equal(parts[1].trim(),'<span>w!lbur</span>');
       })
-    };
+      .then(done, done);
+    });
+  });
 
-    helpers.render(tmpl)(data).then(parts => {
-      assert.equal(parts.length, 2);
-      assert.equal(parts[0].trim(),
-      '<span class="msg">Hello <strong>Wilbur</strong>!</span>');
-      assert.equal(parts[1].trim(),'<span>w!lbur</span>');
+  describe('Falsey', function(){
+    it('basics works', function(done){
+      let tmpl = `
+        <span class="msg">Hello <strong>{{name}}</strong>!</span>
 
-    })
-    .then(done, done);
+        <template if="{{hasUser}}">
+          <span>{{handle}}</span>
+        </template>
+      `;
+
+      let data = {
+        name: 'Wilbur',
+        hasUser: Promise.resolve(false)
+      };
+
+      helpers.render(tmpl)(data).then(parts => {
+        assert.equal(parts.length, 2);
+        assert.equal(parts[0].trim(),
+        '<span class="msg">Hello <strong>Wilbur</strong>!</span>');
+        assert.equal(parts[1].trim(),'', 'Should be just space');
+      })
+      .then(done, done);
+    });
   });
 });
 
