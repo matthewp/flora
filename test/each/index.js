031479835d584f833cd92f17bc4031b604755129
Matthew Phillips <matthew@matthewphillips.info>
2017-03-06 21:01:49 -0500

    closer to have iterables

--- test/each/index.js
+++ test/each/index.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const helpers = require('../helpers');
+const streamify = require('stream-array');
 
 describe('Each', function(){
   describe('With an Array', function(){
@@ -20,6 +21,27 @@ describe('Each', function(){
         assert.equal(parts.length, 1);
         assert.equal(helpers.trim(parts[0]),
         '<div>before</div><li>golf</li><li>tennis</li><div>after</div>');
+      })
+      .then(done, done);
+    });
+  });
+
+  describe.only('With a Stream', function(){
+    it('Chunks out each item', function(done){
+      let tmpl = `
+        <div>before</div>
+        <template each="{{sports}}" as="sport">
+          <li>{{sport}}</li>
+        </template>
+        <div>after</div>
+      `;
+
+      let data = {
+        sports: streamify(['golf','tennis'])
+      };
+
+      helpers.render(tmpl)(data).then(parts => {
+        console.log(parts);
       })
       .then(done, done);
     });
