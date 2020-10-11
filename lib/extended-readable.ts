09c2b0884220c4159cd669d403daab82a3f00f6d
Kuba Orlik <kontakt@kuba-orlik.name>
2020-10-11 10:59:43 +0200

    Change extended readable types

--- lib/extended-readable.ts
+++ lib/extended-readable.ts
@@ -1,7 +1,7 @@
 import { Readable } from "stream";
 
 export default class ExtendedReadable extends Readable {
-	public metadata = {};
+	public metadata: { [field: string]: any } = {};
 	constructor(...args: ConstructorParameters<typeof Readable>) {
 		super(...args);
 	}
