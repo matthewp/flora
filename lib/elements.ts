ce9e01f891f51eccf33e14e581db664c5cd93f9a
Kuba Orlik <kontakt@kuba-orlik.name>
2020-11-09 19:26:30 +0100

    Some new elements

--- lib/elements.ts
+++ lib/elements.ts
@@ -3,6 +3,7 @@ import { html } from ".";
 import ExtendedReadable from "./extended-readable";
 import { map } from "./map";
 import * as fs from "fs";
+import { v4 as uuid } from "uuid";
 
 type StringLike = string | number | boolean | ReadableStream | ExtendedReadable;
 
@@ -185,12 +186,14 @@ export function checkForMore(
 			.check-for-more__container > input {
 				grid-column: 1;
 				grid-row: 1;
+				margin: 5px 0;
 			}
 			.check-for-more__container > .check-for-more__content {
 				grid-column: 1/3;
 			}
 			.check-for-more__container > label {
 				grid-row: 1;
+				margin: 5px 0 1px 0;
 			}
 			.check-for-more__content {
 				display: none;
@@ -224,5 +227,137 @@ export async function cond(
 		return content;
 	} else {
 		return "";
+	}
+}
+
+export class Loader {
+	id: string;
+	private delay = 65;
+	constructor(
+		public color: string = "hsl(268, 40%, 42%)",
+		public message: string = "Please wait..."
+	) {
+		this.id = "loader" + uuid().replace(/-/g, "_");
+	}
+
+	private box(index: number) {
+		return /* HTML */ `<div
+				class="loading-box"
+				style="animation-delay: ${this.delay * index}ms"
+			></div>
+			<div class="loading-shadow"></div>`;
+	}
+
+	private style() {
+		return /* HTML */ `<style>
+			@keyframes jump {
+				0% {
+					transform: translateY(0);
+				}
+				7% {
+					transform: scaleY(0.8) scaleX(1.2) translateY(2px);
+				}
+				10% {
+					transform: scaleY(0.8) scaleX(1.2) translateY(2px);
+				}
+				19% {
+					transform: scaleX(0.9) scaleY(1.1) translateY(-0.5rem);
+				}
+				37% {
+					transform: translateY(-1rem);
+				}
+				55% {
+					transform: scaleX(0.9) scaleY(1.1) translateY(-0.5rem);
+				}
+				70% {
+					transform: scaleY(0.8) scaleX(1.2) translateY(2px);
+				}
+				100% {
+					transform: translateY(0);
+				}
+			}
+			@keyframes shadow-jump {
+				0% {
+					transform: scaleX(1);
+				}
+				7% {
+					transform: scaleX(1.2);
+				}
+				10% {
+					transform: scaleX(1.2);
+				}
+				19% {
+					transform: scaleX(0.9);
+				}
+				37% {
+					transform: scaleX(1);
+				}
+				55% {
+					transform: scaleX(0.9);
+				}
+				70% {
+					transform: scaleX(0.8);
+				}
+				100% {
+					transform: scaleX(1);
+				}
+			}
+
+			.loading-boxes {
+				text-align: center;
+				padding-top: 4rem;
+			}
+
+			.loading-box {
+				width: 1rem;
+				height: 1rem;
+				background-color: ${this.color};
+				display: inline-block;
+				vertical-align: bottom;
+				animation: jump 700ms;
+				animation-iteration-count: infinite;
+				z-index: 1;
+				position: relative;
+			}
+
+			.loading-shadow {
+				display: inline-block;
+				margin-right: 1rem;
+				width: 1rem;
+				margin-left: -1rem;
+				box-shadow: -4px 6px 2px 1px rgba(0, 0, 0, 0.16);
+				/* animation: shadow-jump 700ms;
+				animation-iteration-count: infinite;
+				z-index: 0;
+				position: relative;
+                */
+			}
+
+			.loading-message {
+				margin-top: 10px;
+				text-align: center;
+			}
+		</style> `;
+	}
+
+	public start() {
+		let ret = this.style();
+		ret += `<div class="loading-container" id=${this.id}>`;
+		ret += `<div class="loading-boxes" >`;
+		for (let i = 1; i <= 4; i++) {
+			ret += this.box(i);
+		}
+		ret += `</div>`;
+		ret += `<div class="loading-message">${this.message}</div>`;
+		ret += `</div>`;
+		return ret;
+	}
+
+	public stop() {
+		return /* HTML */ ` <style>
+			#${this.id} {
+				display: none;
+			}
+		</style>`;
 	}
 }
